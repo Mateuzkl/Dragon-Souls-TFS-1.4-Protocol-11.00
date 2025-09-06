ffc4983bcd638e4bb29da381a41d7cacfc056471
Mateuzkl <mateus.roberto@educacao.mg.gov.br>
2025-09-06 09:17:38 -0300

    Fix reward boss summon handling
    Moved reward boss setup logic to default_onSpawn for better organization,
    maintainability, and consistency with other event scripts.

--- data/events/scripts/monster.lua
+++ data/events/scripts/monster.lua
@@ -5,27 +5,7 @@ function Monster:onDropLoot(corpse)
 end
 
 function Monster:onSpawn(position, startup, artificial)
-    if hasEvent.onSpawn then
+ if hasEvent.onSpawn then
         return Event.onSpawn(self, position, startup, artificial)
-    else
-        if not self:getType():canSpawn(position) then
-            return false
-        end
-
-        if self:getType():isRewardBoss() then
-            self:setReward(true)
-        end
-
-        if not startup then
-            local spec = Game.getSpectators(position, false, false)
-            for _, pid in pairs(spec) do
-                local monster = Monster(pid)
-                if monster and not monster:getType():canSpawn(position) then
-                    monster:remove()
-                end
-            end
-        end
-
-        return true
     end
 end
